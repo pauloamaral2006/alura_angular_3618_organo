d8bd290d06443a271b1f1c8bfd99921644da55d2
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2025-09-09 07:51:24 -0300

    Aula 02

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    url: "http://localhost/",
+  },
+  transform: {
+    "^.+\\.(ts|mjs|js|html)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleFileExtensions: ["ts", "html", "js", "json"],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
 };
---
+++ src/app/services/livro.service.spec.ts
@@ -0,0 +1,75 @@
+import { GeneroLiterario, Livro } from '../componentes/livro/livro';
+import { livros } from '../mock-livros';
+import { ErroGeneroLiterario, LivroService } from './livro.service';
+
+describe('LivroService', () => {
+  let service: LivroService;
+
+  beforeEach(() => {
+    service = new LivroService();
+  });
+  it('deveria ser criado', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('deveria adicionar um novo livro', () => {
+    service = new LivroService();
+    const novoLivro: Livro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autor Desconhecido',
+      imagem: 'http://example.com/cover.jpg',
+      genero: { id: 'romance', value: 'Romance' },
+      dataLeitura: '2024-04-19',
+      classificacao: 5,
+    };
+
+    service.adicionarLivro(novoLivro);
+
+    const livrosPorGenero = service.obterLivrosPorGenero('romance');
+
+    expect(livrosPorGenero).toContain(novoLivro);
+  });
+
+  it('deveria recuperar corretamente os livros por gênero', () => {
+    const livrosPorGenero = service.obterLivrosPorGenero('romance');
+
+    const livrosEsperados = livros.filter(
+      (livro) => livro.genero.id === 'romance'
+    );
+
+    expect(livrosPorGenero).toEqual(livrosEsperados);
+  });
+
+  //Código omitido
+
+  it('deveria inicializar os gêneros corretamente', () => {
+    const generosEsperados: GeneroLiterario[] = [
+      { id: 'romance', value: 'Romance' },
+      { id: 'misterio', value: 'Mistério' },
+      { id: 'fantasia', value: 'Fantasia' },
+      { id: 'ficcao-cientifica', value: 'Ficção Científica' },
+      { id: 'tecnicos', value: 'Técnicos' },
+    ];
+
+    expect(service.generos).toEqual(generosEsperados);
+  });
+
+  //Código omitido
+
+  it('deveria lançar um erro ao tentar cadastrar um livro com gênero desconhecido', () => {
+    const novoLivro: Livro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autor Desconhecido',
+      imagem: 'http://example.com/cover.jpg',
+      genero: { id: 'nao-existe', value: 'Não Existe' },
+      dataLeitura: '2024-04-19',
+      classificacao: 5,
+    };
+
+    expect(() => service.adicionarLivro(novoLivro)).toThrow(
+      ErroGeneroLiterario
+    );
+  });
+  //Código omitido
+  /// AAA - arrange act assert
+});
